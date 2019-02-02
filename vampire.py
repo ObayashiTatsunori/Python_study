d5ac7bdd9af5e6f77d9129f9daa5c7c9ff0930d2
Obayashi <4210.tatu@gmail.com>
2019-02-02 22:21:38 +0900

    2.1ブール型～2.7.5break文

---
+++ vampire.py
@@ -0,0 +1,13 @@
+
+name = 'Jon'
+age = 12
+if name == 'Alice':
+  print('やぁ、Alice。')
+elif age<12:
+  print('Aliceじゃないね、お嬢ちゃん')
+elif age>2000:
+  print('Aliceはお前のような不死身じゃない、吸血鬼め')
+elif age>100:
+  print('Aliceじゃないね、お婆ちゃん')
+else:
+  print('君はAliceでも子供でもないね')
---
+++ yourName.py
@@ -0,0 +1,5 @@
+name=''
+while name != 'たつのり':
+  print('あなたの名前を入力してください')
+  name=input()
+print('どうも')
